e103c25354947293cf1f50dd42584fdab34bea11
Davide Filosa <davideclaypool@gmail.com!>
2024-04-05 18:49:22 +0200

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ import React from "react";
 
 const Home = () => {
   return (
-    <main className="flex flex-col w-full  h-full items-center justify-start gap-8 pt-40 bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+    <main className="flex flex-col w-full  h-full items-center justify-start gap-12 pt-40 bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+      <div className="h-1/2 w-full -left-32 fixed top-32 bg-gradient-to-t transition from-purple-500  to-sky-500 -z-10 blur-3xl	opacity-30 dark:opacity-0" />
       <div className="h-full w-full fixed top-1/2 bg-gradient-to-r transition from-purple-500  to-sky-500 -z-10 blur-3xl	opacity-50 dark:opacity-20" />
       <div className="font-bold text-9xl bg-gradient-to-r  from-purple-500  to-sky-500 text-transparent bg-clip-text">
         AiFormBuilder
@@ -16,13 +17,13 @@ const Home = () => {
         </h1>
       </div>
       <div>
-        <h2 className="text-xl font-semibold drak:text-white">
+        <h2 className="text-xl font-semibold text-foreground">
           Generate, Share, and Collect Responses Seamlessly with Our AI-Powered
           Form Building Platform.
         </h2>
       </div>
       <div>
-        <Button size={"lg"} asChild>
+        <Button size={"lg"} asChild className="shadow-md">
           <Link href="/dashboard">
             <SignedIn>Go To the Dashboard</SignedIn>
             <SignedOut>Start for Free</SignedOut>
